215063ef06b7783aace14e249ea9178a59de1c86
Ruben Sutton <ruben@scstudios.tech>
2026-09-12 08:55:53 +1000

    Keep internal review stages private

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ import { shopRoutes } from "./routes/shop.js";
 import { seedStore } from "./seed.js";
 import { createSlackNotifier } from "./slack.js";
 import { createStore } from "./store.js";
-import { formatDate, formatDateTime, jsonArray, readFlash, statusLabel } from "./utils.js";
+import { formatDate, formatDateTime, jsonArray, participantStatusLabel, readFlash, statusLabel } from "./utils.js";
 import { createInternalFrequency } from "./internal-frequency.js";
 
 const consoleLogger = {
@@ -56,6 +56,7 @@ export async function createApp({
   app.locals.formatDate = formatDate;
   app.locals.formatDateTime = formatDateTime;
   app.locals.statusLabel = statusLabel;
+  app.locals.participantStatusLabel = participantStatusLabel;
   app.locals.renderMarkdown = renderMarkdown;
   app.locals.hasPermission = hasPermission;
   app.locals.isOrganizer = isOrganizer;
